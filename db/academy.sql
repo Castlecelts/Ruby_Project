b251bb1c9929e56895667145a67822722b18a5ec
Castlecelts <jamesdrmundie@gmail.com>
2018-11-08 16:21:31 +0000

    academy.sql basic table format

--- db/academy.sql
+++ db/academy.sql
@@ -4,7 +4,19 @@ DROP TABLE heroes
 
 CREATE TABLE heroes(
   id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255),
+  name VARCHAR(255), NOT NULL
   world VARCHAR(255),
   hireable BIT NOT NULL
+);
+
+CREATE TABLE teams(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255), NOT NULL
+  region VARCHAR(255)
+);
+
+CREATE TABLE team_rosters(
+  id SERIAL8 PRIMARY KEY,
+  team_id SERIAL8 REFERENCES teams(id),
+  hero_id SERIAL8 REFERENCES hero(id)
 );
