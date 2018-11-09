eb5cad1d8ca87b0a49b9f933b9aa55fd64da3bac
Castlecelts <jamesdrmundie@gmail.com>
2018-11-09 13:37:38 +0000

    complete  add new hero form

--- Controllers/heroes_controller.rb
+++ Controllers/heroes_controller.rb
@@ -3,7 +3,19 @@ require('sinatra/contrib/all')
 require_relative('../models/hero')
 also_reload('../models/*')
 
+#index
 get '/heroes' do
   @heroes = Hero.all()
   erb( :"heroes/index")
+end
+
+#new
+get '/heroes/new' do
+  erb( :"heroes/new")
+end
+
+#create
+post '/heroes' do
+  Hero.new(params).save
+  redirect to '/heroes'
 end
