d619c4251ed8926301a8a1cd02fb1c58b296dfa5
Castlecelts <jamesdrmundie@gmail.com>
2018-11-12 13:06:25 +0000

    hero dropdown selection for hero_team add, still to fix team dropdown

--- Controllers/hero_teams_controller.rb
+++ Controllers/hero_teams_controller.rb
@@ -1,3 +1,14 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/team')
+require_relative('../models/hero')
+require_relative('../models/hero-team')
+also_reload('../models/*')
+
 get '/hero_teams/new' do
-"Hello World"
+  # new?team_id=<%= @team.id %>
+  # @team = Team.find(params['id'])
+  # p @team
+  @heroes = Hero.all
+  erb(:"hero-teams/new")
 end
