2271f31042b3c923245e35dca1ba201946c363ff
Castlecelts <jamesdrmundie@gmail.com>
2018-11-12 12:10:58 +0000

    link to add new hero to a team created. start of new restful routes

---
+++ Controllers/hero_teams_controller.rb
@@ -0,0 +1,3 @@
+get '/hero_teams/new' do
+"Hello World"
+end
--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require('pry-byebug')
 require_relative('controllers/heroes_controller')
 require_relative('controllers/teams_controller')
+require_relative('controllers/hero_teams_controller')
+
+
 
 
 get '/' do
